17d8077cfde06b2d65e928bebb121204012d9d74
Sagar <sagar.0dev@gmail.com>
2022-01-26 17:07:55 +0530

    40 to 50

--- HealthyProgrammer.py
+++ HealthyProgrammer.py
@@ -1,30 +1,52 @@
 from pygame import mixer
+from datetime import datetime
+from time import time
 
-file = 'faisu.mp3'
-mixer.init()
-mixer.music.load(file)
-mixer.music.play()
-# infinite loop
-while True:
 
-    print("Press 'p' to pause, 'u' to unpause, 's' to start again")
-    print("Press 'e' to exit the program")
-    query = input()
+def musciOnLoop(file, stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        a = input()
+        if a == stopper:
+            mixer.music.stop()
+            break
 
-    if query == 'p':
 
-        # Pausing the music
-        mixer.music.pause()
-    elif query == 'u':
+def log_now(msg):
+    with open("mylogs.txt", "a") as f:
+        f.write(f"{msg} {datetime.now()}\n")
 
-        # Resuming the music
-        mixer.music.unpause()
-    elif query == 's':
-        mixer.music.play()
-    elif query == 'e':
 
-        # Stop the mixer
-        mixer.music.stop()
-        break
-    else:
-        print("Invalid input\nPlease try again from the options given below.")
+if __name__ == '__main__':
+    init_water = time()
+    water_time = 60
+    while True:
+        if time() - init_water > water_time:
+            print("Time to Drink water. Type 'drank' to stop the alarm")
+            musciOnLoop('faisu.mp3', 'drank')
+            init_water = time()
+            log_now('Drank water at')
+
+# started playing music
+# file = 'faisu.mp3'
+# mixer.init()
+# mixer.music.load(file)
+# mixer.music.play()
+#
+# # getting current time
+# now = datetime.now()
+# start_time = now.strftime("%S")
+#
+# input = input("Press 'e' to exit the loop")
+# if input == 'e':
+#
+# lis = [start_time]
+# # infinite loop
+# while True:
+#     t = datetime.now()
+#     current_time = t.strftime("%S")
+#     if abs(int(start_time) - int(current_time)) > 10:
+#         mixer.music.play()
+#         start_time = current_time
