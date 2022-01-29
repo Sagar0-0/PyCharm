d62655a18bd4429e037081812f6fcbdacc5e2a24
Sagar <sagar.0dev@gmail.com>
2022-01-29 13:43:10 +0530

    80 to 90

---
+++ NewsReading.py
@@ -0,0 +1,8 @@
+def read(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+
+if __name__ == '__main__':
+    read('Hello watchers? Are you ready for today exam?')
--- myJSON.py
+++ myJSON.py
@@ -4,3 +4,5 @@ data = '{"sagar":112 ,"akash":211}'
 print(data)
 parsed = json.loads(data)
 print(parsed['akash'])
+
+# json.dump converts a python dict into a json file
