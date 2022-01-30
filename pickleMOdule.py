383484b6530f562e8968c0f281e3500f57ee5747
Sagar <sagar.0dev@gmail.com>
2022-01-30 14:18:33 +0530

    80 to 90

---
+++ pickleMOdule.py
@@ -0,0 +1,22 @@
+
+import pickle
+
+# Pickling a python object
+# cars = ["Audi", "BMW", "Maruti Suzuki", "Harryti Tuzuki"]
+# file = "mycar.pkl"
+# fileobj = open(file, 'wb')
+# pickle.dump(cars, fileobj)
+# fileobj.close()
+
+file = "mycar.pkl"
+fileobj = open(file, 'rb')
+mycar = pickle.load(fileobj)
+print(mycar)
+print(type(mycar))
+
+
+# pickle.loads = ?
+
+
+
+
