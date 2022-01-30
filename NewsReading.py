8be6e8658850f124eba04e14302da10ccb7ff76a
Sagar <sagar.0dev@gmail.com>
2022-01-30 13:55:45 +0530

    80 to 90

--- NewsReading.py
+++ NewsReading.py
@@ -1,8 +1,37 @@
+from win32com.client import Dispatch
+import requests
+import json
+
+
+# helper method to speaks out the input string
 def read(str):
-    from win32com.client import Dispatch
     speak = Dispatch("SAPI.SpVoice")
     speak.Speak(str)
 
 
+# helper method to return the json dictionary from input url
+def getJson(URl):
+    jsonStr = requests.get(URL).text
+    parsed = json.loads(jsonStr)
+    return parsed
+
+
+# helper method to reads out the given articles list
+def readData(articlesList):
+    for article in articlesList:
+        readTitle = f"Title: {article['title']}"
+        read(readTitle)
+        readDesc = f"Description: {article['description']}"
+        read(readDesc)
+
+
+# direct method to read all articles from url
+def readArticles(url):
+    jsonDict = getJson(url)
+    articlesList = jsonDict['articles']
+    readData(articlesList)
+
+
 if __name__ == '__main__':
-    read('Hello watchers? Are you ready for today exam?')
+    URL = "https://newsapi.org/v2/everything?q=marvel&apiKey=cdbba53bb0d34fa2a43789edbdf5feba"
+    readArticles(URL)
